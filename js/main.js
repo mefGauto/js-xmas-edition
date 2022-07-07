cf7ee9d90e211a646de3608860127b8d7b63a864
mefGauto <gautomatiaz@gmail.com>
2022-07-07 12:08:00 -0300

    Continuación del ejercicio segun las intrucciones Se añade la funcion que valida el formulario en general al hacer click en el boton "Enviar Carta" y muestra los errores en la consola. La funcion que validan especificamente cada campo se actualizan para ser mas especificas y aceptar solo letras y numeros con RegEx.

--- js/main.js
+++ js/main.js
@@ -1,34 +1,70 @@
 const $form = document.querySelector('#carta-a-santa');
+$form.onsubmit = validarFormulario;
 
-let nombre = $form.nombre.value;
-let ciudad = $form.ciudad.value;
-let comportamiento = $form.comportamiento.value;
-let descripcionRegalo = $form['descripcion-regalo'].value;
+function validarFormulario (event) {
+
+    let nombre = $form.nombre.value;
+    let ciudad = $form.ciudad.value;
+    let descripcionRegalo = $form['descripcion-regalo'].value;
+
+    validarNombre(nombre);
+    validarCiudad(ciudad);
+    validarDescripcionRegalo(descripcionRegalo)
+    console.log(validarNombre(nombre), validarCiudad(ciudad), validarDescripcionRegalo(descripcionRegalo))
+
+    event.preventDefault();
+
+}
+
+function manejarErrores (errores) {
+
+}
 
 function validarNombre(nombre) {
 
     if (nombre.length === 0){
 
-        return 'Este campo tiene que tener mas de un caracter.';
+        return 'El nombre tiene que tener mas de un caracter.';
     }
     
     if (nombre.length >= 50) {
 
-        return 'Este campo no puede tener mas de cincuenta caracteres.';
+        return 'El nombre no puede tener mas de cincuenta caracteres.';
     }
+
+    if (!/^[A-z]+$/.test(nombre)) {
+        
+        return 'El nombre solo puede tener letras.';
+    }
+    
     return '';
 
 }
 
 function validarCiudad(ciudad) {
 
-    if (ciudad === '') {
+    if (ciudad.length === 0) {
         return 'Debes elegir una ciudad.';
+    }else {
+        return '';
     }
-    return '';
 }
 
-function validarComportamiento(comportamiento) {
+function validarDescripcionRegalo(descripcionRegalo) {
 
-    
+    if (descripcionRegalo.length === 0) {
+
+        return 'Agrega una descripcion del regalo.';
+    }
+
+    if (descripcionRegalo.length > 100) {
+        return 'La descripcion del regalo no puede tener mas de 100 caracteres.';
+    }
+
+    if (!/^[a-z0-9]+$/i.test(descripcionRegalo)) {
+        return 'La descripcion del regalo solo puede tener letras y numeros.';
+    }
+
+
+    return '';
 }
--- js/pruebas.js
+++ js/pruebas.js
@@ -1,27 +1,68 @@
 function probarValidarNombre() {
   console.assert(
-      validarNombre('') === 'Este campo tiene que tener mas de un caracter.',
-      'Validar nombre no validó que el nombre no sea vacío',
+      validarNombre('') === 'El nombre tiene que tener mas de un caracter.',
+        'Validar nombre no validó que el nombre no sea vacío',
   );
 
   console.assert(
+      
       validarNombre(
         '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-        'Este campo no puede tener mas de cincuenta caracteres.',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
+        'El nombre no puede tener mas de cincuenta caracteres.',
+        'Validar nombre no validó que el nombre sea menor a 50 caracteres'
+  );
+
+  console.assert(
+      validarNombre('812938') === 'El nombre solo puede tener letras.',
+        'Validar nombre no validó que el nombre solo contenga letras'
+
+  )
+ 
+  console.assert(
+      
+      validarNombre('Matias') === '',
+        'Validar nombre no se ejecuto con un nombre válido'
   );
 }
-
-probarValidarNombre();
-
 
 function probarValidarCiudad() {
 
     console.assert(
         
         validarCiudad('') === 'Debes elegir una ciudad.',
-        'Validar ciudad no valido que el usuario haya elegido una ciudad'
+        'No se validó que el usuario haya elegido una ciudad'
     )
 
+    console.assert(
+
+      validarCiudad('Buenos Aires') === '',
+        'Validar ciudad no se ejecutó con una ciudad válida'
+    );
 }
+
+function probarValidarDescripcionRegalo() {
+
+  console.assert(
+    validarDescripcionRegalo('') === 'Agrega una descripcion del regalo.',
+      'No se validó que la descripción del regalo no este vacia'
+  )
+
+  console.assert(
+
+    validarDescripcionRegalo(
+      'jksajdksadlksjakldjsakldjlkasjdkljaskldjlkasjdljdlksahksahjdfsajkldjksajdlksajdklasjdklasjdlkasjldkjasldjklajlkjaslkd')
+      === 'La descripcion del regalo no puede tener mas de 100 caracteres.', 
+      'No se validó que el usuario haya puesto menos de 100 caracteres en la descripción del regalo'
+
+  );
+
+  console.assert(
+    validarDescripcionRegalo(',.,.,.,') === 'La descripcion del regalo solo puede tener letras y numeros.',
+    'No se validó que el usuario solo haya usando letras y numeros en la descripción del regalo.'
+  );
+}
+
+
+probarValidarNombre();
 probarValidarCiudad();
+probarValidarDescripcionRegalo();
